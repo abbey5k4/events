fff36014382ced971ffc9acdbc5adc98127c8bd3
adetayo abiodun <adetayo.abiodun@sterling.ng>
2022-08-25 23:39:35 +0100

    footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,18 @@
-import '../styles/globals.css';
+import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>Pace - Events</title>
+        <meta name="description" content="Pace Events" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
