9a5430c9c672b432c3a697fa80d46d4b4f2f2ac1
adetayo abiodun <adetayo.abiodun@sterling.ng>
2022-08-26 00:56:57 +0100

    featured cards without slick

---
+++ src/components/FeaturedCard/index.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import styles from "./index.module.css";
+import { BsBookmark, BsBookmarkFill } from "react-icons/bs";
+import { ImLocation2 } from "react-icons/im";
+
+const FeaturedCard = () => {
+  const [bookmark, setBookmark] = useState(false);
+
+  return (
+    <div className="py-3">
+      <div className={`card ${styles.cardBg}`}>
+        <div className="card-body">
+          <div className="card-title">
+            <div className="d-flex justify-content-between">
+              <div></div>
+              <div className={`bg-white ${styles.badgeDiv}`}>
+                <p>19</p>
+                <p>APR</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="px-1 text-white pt-5 pb-2">
+              <h6 className={styles.cardContent}>
+                American Association of Neurological Surgeons (AANS) 2021 Annual
+                Scientific Meeting
+              </h6>
+
+              <div className="d-flex justify-content-between pt-2">
+                <div className="d-flex">
+                  <ImLocation2 className={styles.iconStyle} />
+                  <p className={styles.location}>Medical House, SK, Canada</p>
+                </div>
+                <div className="" onClick={() => setBookmark(!bookmark)}>
+                  {bookmark ? (
+                    <BsBookmarkFill className={styles.iconStyleTwo} />
+                  ) : (
+                    <BsBookmark className={styles.iconStyleTwo} />
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedCard;
--- src/container/LandingPage/index.js
+++ src/container/LandingPage/index.js
@@ -1,12 +1,31 @@
-import React from 'react'
-import Footer from '../../constants/Footer';
+import React from "react";
+import FeaturedCard from "../../components/FeaturedCard";
+import Footer from "../../constants/Footer";
 
 const LandingPageContainer = () => {
-    return (
-        <div>
-            <Footer />
+  return (
+    <div>
+      <div className="container">
+        <h5>Featured Events</h5>
+        <div className="row">
+          <div className="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
+            <FeaturedCard />
+          </div>
+          <div className="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
+            <FeaturedCard />
+          </div>
+          <div className="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
+            <FeaturedCard />
+          </div>
+          <div className="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
+            <FeaturedCard />
+          </div>
         </div>
-    )
-}
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
 
 export default LandingPageContainer;
